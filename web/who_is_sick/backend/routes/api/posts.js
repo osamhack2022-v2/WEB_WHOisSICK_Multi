5f789b1f3f43a6010ed2a435f0ddfd2422bef364
skagur10 <101463561+skagur10@users.noreply.github.com>
2022-10-02 23:47:27 +0000

    second commit

--- web/who_is_sick/backend/routes/api/posts.js
+++ web/who_is_sick/backend/routes/api/posts.js
@@ -4,4 +4,7 @@ const router = express.Router();
 
 router.get('/',(req, res) => res.send('posts router'));
 
+
+
+
 module.exports = router;
--- web/who_is_sick/backend/server.js
+++ web/who_is_sick/backend/server.js
@@ -12,11 +12,11 @@ app.use(express.json({
     extended: false,
 }));
 
-app.use('api/auth',require('./routes/api/auth'));
-app.use('api/posts',require('./routes/api/posts'));
-app.use('api/profile',require('./routes/api/profile'));
-app.use('api/user',require('./routes/api/user'));
+app.use('/api/auth',require('./routes/api/auth'));
+app.use('/api/posts',require('./routes/api/posts'));
+app.use('/api/profile',require('./routes/api/profile'));
+app.use('/api/user',require('./routes/api/user'));
 
-const PORT = process.env.PORT || 5500;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, ()=>console.log(`Server starts on post ${PORT}`));

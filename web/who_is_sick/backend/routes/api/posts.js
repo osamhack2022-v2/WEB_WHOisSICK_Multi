bb23c9ef8ee4d1198a1c09e7d622efe29ff9ea64
서한유 <hu8232@naver.com>
2022-10-05 14:34:25 +0000

    hihi

--- web/who_is_sick/backend/routes/api/posts.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-
-router.get('/',(req, res) => res.send('posts router'));
-
-module.exports = router;
--- web/wis/src/App.js
+++ web/wis/src/App.js
@@ -1,11 +1,20 @@
 import './App.css';
 import Layout from './components/shared/Layout';
+import {useState} from "react";
 
 function App() {
-
+  const [isTrue, setIsTrue] = useState(false);
+  function onClick() {
+    setIsTrue(!isTrue);
+  }
   return (
     <Layout>
-      <div>콘텐츠!</div>
+      <div>
+        <button onClick={onClick}>True?</button>
+        {isTrue && <h1>True!</h1>}
+        {!(isTrue) && <h1>false</h1>}
+      </div>
+     
     </Layout>
   );
 }

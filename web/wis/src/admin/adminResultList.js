cad8491336c120ab95944548fd4e2799cd94b16f
superjoung <46063485+superjoung@users.noreply.github.com>
2022-10-30 01:45:18 +0000

    markdown

--- web/wis/src/admin/adminResultList.js
+++ web/wis/src/admin/adminResultList.js
@@ -42,8 +42,6 @@ function Row(props) {
 
   const [serverValues, setServerValue] = React.useState({
     _id : row._id,
-    ok: 3,
-    day : "",
     inter : "",
   });
 
@@ -65,36 +63,23 @@ function Row(props) {
 
   const handleChange = (event) => {
     const { name, value } = event.target;
-      if(checked) {
-        setServerValue({
-          ...serverValues,
-          [name]:4
-      });
-      }
-      else {
-        setServerValue({
-          ...serverValues,
-          [name]:5
-      });
-      }
-      
-    if(name !== "ok") {
-      setServerValue({
-        ...serverValues,
-        [name]:value
+  
+    setServerValue({
+      ...serverValues,
+      [name]:value
     });
-    }
   }
 
   const handleSudmit = (event) => {
     event.preventDefault();
 
     const date = new Date();
-    const dayString = date.toLocaleDateString('ko-kr');
-    setServerValue({
-      ...serverValues,
-      day:dayString
-  });
+    const day = date.toLocaleDateString('ko-kr');
+    let ok = 3;
+    if(checked)
+      ok = 4;
+    else
+      ok = 5;
 
   fetch('http://127.0.0.1:5000/main/result', {
   credentials: 'include',    
@@ -103,7 +88,10 @@ function Row(props) {
       'content-Type': 'application/json',
   },
   body: JSON.stringify(
-    serverValues
+    serverValues, {
+      day,
+      ok,
+    }
   ),
 })
     setState({ openSnack: true,  vertical: 'top', horizontal: 'center',});

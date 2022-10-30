56b3ad28a756352e7c97a2ee93dd4dac0fb2b321
skagur10 <101463561+skagur10@users.noreply.github.com>
2022-10-30 00:02:42 +0000

    save61

--- web/wis/src/login/logInForm.js
+++ web/wis/src/login/logInForm.js
@@ -23,7 +23,7 @@ const MuiLoginForm = () => {
 
     const onSubmitHandler = (event) => {
         event.preventDefault();
-        const sn = values.id;
+        const sn = values.sn;
         const password = values.password;
         fetch('http://127.0.0.1:5000/', {
             credentials: 'include',    
